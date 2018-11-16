f00e2855ac4a78e4a0852e062e4e8a8f41bdaebe
Dmitry Torba <dmitrytorba@gmail.com>
2018-11-15 16:10:47 -0800

    Room

---
+++ smartapps/dt/room.src/room.groovy
@@ -0,0 +1,41 @@
+/**
+ *  room
+ *
+ *  Copyright 2018 dt
+ *
+ */
+definition(
+    name: "room",
+    namespace: "dt",
+    author: "dt",
+    description: "room controler",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
